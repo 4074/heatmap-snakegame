b6e7c62210c552ea691999cb528d624a9573c96c
dengwenfeng <dwfn3398@corp.netease.com>
2016-02-11 21:05:33 +0800

    snake初始化

---
+++ js/game.js
@@ -0,0 +1,141 @@
++ function ($){
+    
+    var Y_AXIS_MAX = 7
+    
+    var SnakeGame = function($svg, option){
+        this.$svg = $svg
+        this.options = option
+        this.interval = null
+        
+        this.init()
+    }
+    
+    SnakeGame.defaults = {
+        colors: {
+            background: '#eee',
+            snake: '#8cc665'
+        }
+    }
+    
+    SnakeGame.prototype.init = function(){
+        this.$svg.find('rect.day').attr('fill', this.options.colors.background)
+        this.$grids = this.findGrids()
+        
+        this.snake = {
+            color: this.options.colors.snake,
+            axis: [[0,0], [4,0]],
+            direction: 'right'
+        }
+        
+        this.startGame()
+    }
+    
+    SnakeGame.prototype.findGrids = function(){
+        var $g = this.$svg.find('g')
+        var $rects = []
+        $g.each(function(){
+            var $r = $(this).find('rect')
+            if($r.length === Y_AXIS_MAX){
+                $r.each(function(){
+                    $rects.push($(this))
+                })
+            }
+        })
+        return $rects
+    }
+    
+    SnakeGame.prototype.startGame = function(){
+        //this.createSnake()
+        var self = this
+        this.renderSnake()
+        
+        this.interval = setInterval(function(){
+            self.moveSnake()
+        }, 1000)
+    }
+    
+    SnakeGame.prototype.createSnake = function(){
+        var snakeLength = 4
+        var i = 0
+        while(i < snakeLength){
+            this.$grids[i].attr('fill', this.options.colors.snake)
+            i++
+        }
+    }
+    
+    SnakeGame.prototype.renderSnake = function(){
+        if(!this.snake || !this.snake.axis) return;
+        
+        var axis = this.snake.axis
+        var x1, y1, x2, y2
+        for(var i=0; i<axis.length; i++){
+            if(typeof x1 === 'undefined'){
+                x1 = axis[i][0]
+                y1 = axis[i][1]
+            }else{
+                if(typeof x2 !== 'undefined'){
+                    x1 = x2
+                    y1 = y2
+                }
+                
+                x2 = axis[i][0]
+                y2 = axis[i][1]
+                
+                if(x1 === x2){
+                    var s = Math.min(y1, y2)
+                    var e = Math.max(y1, y2)
+                    
+                    while(s <= e){
+                        this.fillGrid(x1, s, this.options.colors.snake)
+                        s++
+                    }
+                        
+                }else if(y1 === y2){
+                    var s = Math.min(x1, x2)
+                    var e = Math.max(x1, x2)
+                    
+                    while(s <= e){
+                        this.fillGrid(s, y1, this.options.colors.snake)
+                        s++
+                    }
+                }
+            }
+        }
+    }
+    
+    SnakeGame.prototype.moveSnake = function(){
+        var lastAxis = []
+        switch (this.snake.direction) {
+            case 'right':
+                var axis = this.snake.axis
+                
+                lastAxis = axis[0].slice()
+                axis[0][0]++
+                axis[axis.length - 1][0]++
+            case 'left':
+            case 'top':
+            case 'buttom':
+            default:
+                break
+        }
+        
+        console.log(lastAxis)
+        this.fillGrid(lastAxis[0], lastAxis[1], this.options.colors.background)
+        this.renderSnake()
+    }
+    
+    SnakeGame.prototype.fillGrid = function(x, y, color){
+        var grid = this.$grids[Y_AXIS_MAX * x + y]
+        if(grid && grid.attr('fill') !== color){
+            grid.attr('fill', color)
+        }
+    }
+    
+    $.snakeGame = function($el, option){
+        var options = $.extend({}, SnakeGame.defaults, option)
+        console.log(options)
+        new SnakeGame($el, options)
+    }
+    
+}(Zepto)
+
--- js/main.js
+++ js/main.js
@@ -1,34 +1,4 @@
 + function ($){
-    
-    var SnakeGame = function($svg, option){
-        this.$svg = $svg
-        this.options = option
-        
-        this.init()
-    }
-    
-    SnakeGame.defaults = {
-        colors: {
-            background: '#eee',
-            snake: '#8cc665'
-        }
-    }
-    
-    SnakeGame.prototype.init = function(){
-        this.$svg.find('rect.day').attr('fill', this.options.colors.background)
-        
-        this.snake = {
-            color: this.options.color.snake,
-            axia: [(0,0), (0,4)]
-        }
-    }
-    
-    
-    $.snakeGame = function($el, option){
-        var options = $.extend({}, SnakeGame.defaults, option)
-        new SnakeGame($el, options)
-    }
-    
     var $svg = $('#contributions-calendar .js-calendar-graph-svg')
     
     if(!$svg.length){
